35706721f3eb73bdcb7dbeee423747ebb588eda4
tmdsiddl <119027362+tmdsiddl@users.noreply.github.com>
2022-12-09 13:28:11 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 var song;
 var button;
+var jumpButton;
+
 function setup() {
     createCanvas(200, 200);
     song = loadSound('rose.mp3', loaded);
     button = createButton('play');
     button.mousePressed(togglePlaying);
+    jumpButton = createButton('jump');
+    jumpButton.mousePressed(jumpSong);
     background(51);
 
     function togglePlaying() {
@@ -16,9 +20,34 @@ function setup() {
             song.stop();
             button.html('play');
         }
+
+        function changeBackground(col) {
+            background(col);
+            }
+        
+        function jumpSong() {
+        var len = song.duration();
+           var t = 0; //random(len);
+        console.log(t);
+        song.jump(t);
+        }
+
+        function draw() {
+            //if (song.currentTime() > 5) {
+            //background(song.currentTime() * 10, 0, 255);
+            //}
+            }
+
+            
+    song.addCue(2, changeBackground, color(0, 0, 255));
+    song.addCue(4, changeBackground, color(0, 255, 255));
+    song.addCue(6, changeBackground, color(255, 255, 255));
+
+        
     }
 
     function loaded(){
         console.log('loaded');
     } 
 }
+
