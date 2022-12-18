ba6995f8456c8cd82bec49b4de4e3aca471d4022
tmdsiddl <119027362+tmdsiddl@users.noreply.github.com>
2022-12-18 20:44:48 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,79 @@
-var song;
-var button;
+let musicFile;
+let musicFile2;
+let button;
+let button2;
+let button3;
+let button4
 var jumpButton;
-function setup() {
-    createCanvas(200, 200);
-    song = loadSound('rose.mp3', loaded);
-    button = createButton('play');
-    button.mousePressed(togglePlaying);
-    jumpButton = createButton('jump');
-    jumpButton.mousePressed(jumpSong);
-    background(51);
-    }
-    function togglePlaying() {
-        if (!song.isPlaying()) {
-            song.play();
-            song.setVolume(0.3);
+var t=0;
+let amp;
+var rB;
+let music;
+
+function setup()
+{
+    createCanvas(640, 480);
+    soundFormats('mp3', 'ogg');
+    musicFile = loadSound('music', loadMusic);
+    musicFile2 = loadSound('music2', loadMusic);
+
+    button = createButton("play");
+    button.mousePressed(togglePlaying1);
+
+    button2 = createButton("Music1");
+    button2.mousePressed(togglePlaying2);
+    music = 1;
+
+    amp = new p5.Amplitude();
+}
+function togglePlaying1(){
+    if(music === 1){
+        if(!musicFile.isPlaying()){
+            // rB=musicFile.reverseBuffer();
+            musicFile.setVolume(0.5);
+            musicFile.rate(1);
+            musicFile.play();
             button.html('pause');
-        }else {
-            song.stop();
+        }else{
+            musicFile.stop();
             button.html('play');
         }
     }
-        function changeBackground(col) {
-            background(col);
-            }
-        
-        function jumpSong() {
-        var len = song.duration();
-           var t = 0; //random(len);
-        console.log(t);
-        song.jump(t);
+    if(music === 2){
+        if(!musicFile2.isPlaying()){
+            // rB=musicFile.reverseBuffer();
+            musicFile2.jump(200);
+            musicFile2.setVolume(0.5);
+            musicFile.rate(1);
+            musicFile2.play();
+            button.html('pause');
+        }else{
+            musicFile2.stop();
+            button.html('play');
         }
+    }
+ }
 
+ function togglePlaying2(){
+    if(music === 1){
+        music = 2;
+        button2.html('Music2');
+    }else{
+        music = 1;
+        button2.html('Music1');
+    }
+    console.log(music);
+ }
 
-    song.addCue(2, changeBackground, color(0, 0, 255));
-    song.addCue(4, changeBackground, color(0, 255, 255));
-    song.addCue(6, changeBackground, color(255, 255, 255));
-    song.addCue(2, changeBackground, color(0, 255, 255));
-    song.addCue(4, changeBackground, color(255, 0, 255));
-    song.addCue(6, changeBackground, color(255, 255, 0));
+function loadMusic()
+{
+    console.log("It's Working");
+}
 
+function draw(){
+console.log(amp.getLevel(), musicFile.duration());
 
-    
+fill(255, 0, 0);
+ellipse(musicFile.currentTime()*20,480-amp.getLevel()*1000, 20, 20);
+
+}
